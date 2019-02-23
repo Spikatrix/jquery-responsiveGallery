aefc49c6320f8a4dd53bf1ac4c6293c596f1f032
Lonnon Foster <lonnon@lonnon.com>
2019-02-22 16:57:24 -0800

    Eradicate tabs, clean up comment spacing

--- src/jquery.responsiveGallery.js
+++ src/jquery.responsiveGallery.js
@@ -1,9 +1,9 @@
-/*!     
-        jquery.responsiveGallery.js
-        v 1.0
-        David
-        http://www.CodingSerf.com
-        */
+/*
+  jquery.responsiveGallery.js
+  v 1.0
+  David
+  http://www.CodingSerf.com
+*/
 
 ;(function($){
   $.fn.responsiveGallery = function(option){
@@ -22,44 +22,42 @@
 
     function getTransform3dCSS(tx,ty,ry,zIndex,opacity,visibility){
       return {
-        '-webkit-transform'	: 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
-        '-moz-transform'	: 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
-        '-o-transform'		: 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
-        '-ms-transform'		: 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
-        'transform'			: 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
-        'z-index'			: zIndex,
-        'opacity'			: opacity,
-        'visibility'		: visibility
+        '-webkit-transform' : 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
+        '-moz-transform'    : 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
+        '-o-transform'      : 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
+        '-ms-transform'     : 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
+        'transform'         : 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
+        'z-index'           : zIndex,
+        'opacity'           : opacity,
+        'visibility'        : visibility
       };
     }
 
     function getTransform2dCSS(t,s,originX,originY,opacity,visibility){
       return {
-        '-webkit-transform'	: 'translate('+t+') scale('+s+')',
-        '-moz-transform'	: 'translate('+t+') scale('+s+')',
-        '-o-transform'		: 'translate('+t+') scale('+s+')',
-        '-ms-transform'		: 'translate('+t+') scale('+s+')',
-        'transform'			: 'translate('+t+') scale('+s+')',
-
-        '-webkit-transform-origin'	: originX+' '+originY,
-        '-moz-transform-origin'		: originX+' '+originY,
-        '-ms-transform-origin'		: originX+' '+originY,
-        '-o-transform-origin'		: originX+' '+originY,
-        'transform-origin'			: originX+' '+originY,
-
-        'opacity'			: opacity,
-        'visibility'		: visibility
+        '-webkit-transform'        : 'translate('+t+') scale('+s+')',
+        '-moz-transform'           : 'translate('+t+') scale('+s+')',
+        '-o-transform'             : 'translate('+t+') scale('+s+')',
+        '-ms-transform'            : 'translate('+t+') scale('+s+')',
+        'transform'                : 'translate('+t+') scale('+s+')',
+        '-webkit-transform-origin' : originX+' '+originY,
+        '-moz-transform-origin'    : originX+' '+originY,
+        '-ms-transform-origin'     : originX+' '+originY,
+        '-o-transform-origin'      : originX+' '+originY,
+        'transform-origin'         : originX+' '+originY,
+        'opacity'                  : opacity,
+        'visibility'               : visibility
       };
     }
 
     function getTransitionCSS(time,ease){
       return {
-        '-webkit-transition': 'all '+time+'s '+ease,
-        '-moz-transition': 'all '+time+'s '+ease,
-        '-ms-transition': 'all '+time+'s '+ease,
-        '-o-transition': 'all '+time+'s '+ease,
-        'transition': 'all '+time+'s '+ease,
-      }
+        '-webkit-transition' : 'all '+time+'s '+ease,
+        '-moz-transition'    : 'all '+time+'s '+ease,
+        '-ms-transition'     : 'all '+time+'s '+ease,
+        '-o-transition'      : 'all '+time+'s '+ease,
+        'transition'         : 'all '+time+'s '+ease
+      };
     }
 
     function getTransform7CSS(){
@@ -67,31 +65,27 @@
 
       if(support3d){
         cssArray = [
-          getTransform3dCSS('-100%',	-400,	40,		-1,	0,	'hidden'),
-          getTransform3dCSS('0',		-350,	45,		-1,	1,	'visible'),
-
-          getTransform3dCSS('100%', 	-300, 	45,		0,	1,	'visible'),
-          getTransform3dCSS('200%', 	-200, 	40,		1,	1,	'visible'),
-          getTransform3dCSS('300%', 	0, 		0,		2,	1,	'visible'),
-          getTransform3dCSS('400%', 	-200, 	-40,	1,	1,	'visible'),
-          getTransform3dCSS('500%', 	-300, 	-45,	0,	1,	'visible'),
-
-          getTransform3dCSS('600%', 	-350, 	-45,	-1,	1,	'visible'),
-          getTransform3dCSS('700%', 	-400, 	-40,	-1,	0,	'hidden')
+          getTransform3dCSS('-100%', -400,  40, -1, 0, 'hidden'),
+          getTransform3dCSS(    '0', -350,  45, -1, 1, 'visible'),
+          getTransform3dCSS( '100%', -300,  45,  0, 1, 'visible'),
+          getTransform3dCSS( '200%', -200,  40,  1, 1, 'visible'),
+          getTransform3dCSS( '300%',    0,   0,  2, 1, 'visible'),
+          getTransform3dCSS( '400%', -200, -40,  1, 1, 'visible'),
+          getTransform3dCSS( '500%', -300, -45,  0, 1, 'visible'),
+          getTransform3dCSS( '600%', -350, -45, -1, 1, 'visible'),
+          getTransform3dCSS( '700%', -400, -40, -1, 0, 'hidden')
         ];
       } else if(support2d){
         cssArray = [
-          getTransform2dCSS('-100%',	0.6,	'100%',		'50%',	0,	'hidden'),
-          getTransform2dCSS('0',		0.6,	'100%',		'50%',	1,	'visible'),
-
-          getTransform2dCSS('100%', 	0.7,	'100%',		'50%',	1,	'visible'),
-          getTransform2dCSS('200%', 	0.8,	'50%',		'50%',	1,	'visible'),
-          getTransform2dCSS('300%', 	1,		'50%',		'50%',	1,	'visible'),
-          getTransform2dCSS('400%', 	0.8,	'50%',		'50%',	1,	'visible'),
-          getTransform2dCSS('500%', 	0.7,	'0%',		'50%',	1,	'visible'),
-
-          getTransform2dCSS('600%', 	0.6,	'0%',		'50%',	1,	'visible'),
-          getTransform2dCSS('700%', 	0.6,	'0%',		'50%',	0,	'hidden')
+          getTransform2dCSS('-100%', 0.6, '100%', '50%', 0, 'hidden'),
+          getTransform2dCSS(    '0', 0.6, '100%', '50%', 1, 'visible'),
+          getTransform2dCSS( '100%', 0.7, '100%', '50%', 1, 'visible'),
+          getTransform2dCSS( '200%', 0.8,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '300%',   1,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '400%', 0.8,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '500%', 0.7,   '0%', '50%', 1, 'visible'),
+          getTransform2dCSS( '600%', 0.6,   '0%', '50%', 1, 'visible'),
+          getTransform2dCSS( '700%', 0.6,   '0%', '50%', 0, 'hidden')
         ];
       }
 
@@ -103,27 +97,23 @@
 
       if(support3d){
         cssArray = [
-          getTransform3dCSS('-100%',	-400, 	45,		-1,	0,	'hidden'),
-
-          getTransform3dCSS('0', 		-300, 	45,		0,	1,	'visible'),
-          getTransform3dCSS('100%', 	-200, 	45,		1,	1,	'visible'),
-          getTransform3dCSS('200%', 	0, 		0,		2,	1,	'visible'),
-          getTransform3dCSS('300%', 	-200, 	-45,	1,	1,	'visible'),
-          getTransform3dCSS('400%', 	-300, 	-45,	0,	1,	'visible'),
-
-          getTransform3dCSS('500%', 	-400, 	-45,	-1,	0,	'hidden')
+          getTransform3dCSS('-100%', -400,  45, -1, 0, 'hidden'),
+          getTransform3dCSS(    '0', -300,  45,  0, 1, 'visible'),
+          getTransform3dCSS( '100%', -200,  45,  1, 1, 'visible'),
+          getTransform3dCSS( '200%',    0,   0,  2, 1, 'visible'),
+          getTransform3dCSS( '300%', -200, -45,  1, 1, 'visible'),
+          getTransform3dCSS( '400%', -300, -45,  0, 1, 'visible'),
+          getTransform3dCSS( '500%', -400, -45, -1, 0, 'hidden')
         ];
       } else if(support2d){
         cssArray = [
-          getTransform2dCSS('-100%',	0.6,	'100%',		'50%',	0,	'hidden'),
-
-          getTransform2dCSS('0', 		0.7,	'100%',		'50%',	1,	'visible'),
-          getTransform2dCSS('100%', 	0.8,	'50%',		'50%',	1,	'visible'),
-          getTransform2dCSS('200%', 	1,		'50%',		'50%',	1,	'visible'),
-          getTransform2dCSS('300%', 	0.8,	'50%',		'50%',	1,	'visible'),
-          getTransform2dCSS('400%', 	0.7,	'0%',		'50%',	1,	'visible'),
-
-          getTransform2dCSS('500%', 	0.6,	'0%',		'50%',	0,	'hidden')
+          getTransform2dCSS('-100%', 0.6, '100%', '50%', 0, 'hidden'),
+          getTransform2dCSS(    '0', 0.7, '100%', '50%', 1, 'visible'),
+          getTransform2dCSS( '100%', 0.8,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '200%',   1,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '300%', 0.8,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '400%', 0.7,   '0%', '50%', 1, 'visible'),
+          getTransform2dCSS( '500%', 0.6,   '0%', '50%', 0, 'hidden')
         ];
       }
 
@@ -135,23 +125,19 @@
 
       if(support3d){
         cssArray = [
-          getTransform3dCSS('-100%',	-400, 	45,		0,	0,	'hidden'),
-
-          getTransform3dCSS('0', 		-300, 	45,		1,	1,	'visible'),
-          getTransform3dCSS('100%', 	0, 		0,		2,	1,	'visible'),
-          getTransform3dCSS('200%', 	-300, 	-45,	1,	1,	'visible'),
-
-          getTransform3dCSS('300%', 	-400, 	-45,	0,	0,	'hidden')
+          getTransform3dCSS('-100%', -400,  45, 0, 0, 'hidden'),
+          getTransform3dCSS(    '0', -300,  45, 1, 1, 'visible'),
+          getTransform3dCSS( '100%',    0,   0, 2, 1, 'visible'),
+          getTransform3dCSS( '200%', -300, -45, 1, 1, 'visible'),
+          getTransform3dCSS( '300%', -400, -45, 0, 0, 'hidden')
         ];
       } else if(support2d){
         cssArray = [
-          getTransform2dCSS('-100%',	0.65,	'100%',		'50%',	0,	'hidden'),
-
-          getTransform2dCSS('0', 		0.8,	'50%',		'50%',	1,	'visible'),
-          getTransform2dCSS('100%', 	1,		'50%',		'50%',	1,	'visible'),
-          getTransform2dCSS('200%', 	0.8,	'50%',		'50%',	1,	'visible'),
-
-          getTransform2dCSS('300%', 	0.65,	'0%',		'50%',	0,	'hidden')
+          getTransform2dCSS('-100%', 0.65, '100%', '50%', 0, 'hidden'),
+          getTransform2dCSS(    '0',  0.8,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '100%',    1,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '200%',  0.8,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '300%', 0.65,   '0%', '50%', 0, 'hidden')
         ];
       }
 
@@ -163,19 +149,15 @@
 
       if(support3d){
         cssArray = [
-          getTransform3dCSS('-100%',	-300, 	45,		0,	0,	'hidden'),
-
-          getTransform3dCSS('0%', 	0, 		0,		2,	1,	'visible'),
-
-          getTransform3dCSS('100%', 	-300, 	-45,	0,	0,	'hidden')
+          getTransform3dCSS('-100%', -300,  45, 0, 0, 'hidden'),
+          getTransform3dCSS(   '0%',    0,   0, 2, 1, 'visible'),
+          getTransform3dCSS( '100%', -300, -45, 0, 0, 'hidden')
         ];
       } else if(support2d){
         cssArray = [
-          getTransform2dCSS('-100%',	0.65,	'100%',		'50%',	0,	'hidden'),
-
-          getTransform2dCSS('0', 	1,		'50%',		'50%',	1,	'visible'),
-
-          getTransform2dCSS('100%', 	0.65,	'0%',		'50%',	0,	'hidden')
+          getTransform2dCSS('-100%', 0.65, '100%', '50%', 0, 'hidden'),
+          getTransform2dCSS(    '0',    1,  '50%', '50%', 1, 'visible'),
+          getTransform2dCSS( '100%', 0.65,   '0%', '50%', 0, 'hidden')
         ];
       }
 
@@ -212,7 +194,7 @@
 
       setTimeout(function(){
         isAnimating = false;
-      },animatDuration); //防止连击
+      },animatDuration);  // 防止连击
     }
 
     opts.$btn_next.on('click',function(e){
@@ -231,27 +213,27 @@
       var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0],
         touchGap = touch.pageX - touchX;
 
-      if(touchGap>5){ //swipe right
+      if(touchGap>5){  // swipe right
         opts.$btn_prev.trigger('click');
       }
-      if(touchGap<-5){ //swipe left
+      if(touchGap<-5){  // swipe left
         opts.$btn_next.trigger('click');
       }
 
     });
     $(window).on('resize', function(e){
-      $rgItems.removeAttr('style'); //清除样式，否则在resize的时候不能正常取到下列元素的width
+      $rgItems.removeAttr('style');  // 清除样式，否则在resize的时候不能正常取到下列元素的width
 
       var wrapperWidth = $rgWrapper.width(),
         itemWidth = $rgItems.eq(0).width();
 
       rgShowCount = Math.round(wrapperWidth/itemWidth);
 
-      if(rgShowCount === 1){ //显示1个
+      if(rgShowCount === 1){  // 显示1个
         rgTansCSS = getTransform1CSS();
-      } else if(rgShowCount === 3){ //显示3个
+      } else if(rgShowCount === 3){  // 显示3个
         rgTansCSS = getTransform3CSS();
-      } else if(rgShowCount === 5){ //显示5个
+      } else if(rgShowCount === 5){  // 显示5个
         rgTansCSS = getTransform5CSS();
       } else if(rgShowCount === 7){
         rgTansCSS = getTransform7CSS();
@@ -263,15 +245,15 @@
       moveGallery(0);
       setTimeout(function(){
         $rgItems.css(getTransitionCSS(animatDuration/1000, 'ease-in-out'));
-      },10); //等初始位置设置好后再添加动画
+      },10);  // 等初始位置设置好后再添加动画
 
     }).trigger('resize');
 
-    //链式返回
+    // 链式返回
     return this;
   };
   $.fn.responsiveGallery.defaults = {
-    animatDuration: 400, //动画时长 单位 ms
+    animatDuration: 400,  // 动画时长 单位 ms
     $btn_prev: $('.responsiveGallery-btn_prev'),
     $btn_next: $('.responsiveGallery-btn_next')
   };

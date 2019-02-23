6dace67ed978a009175c6e0b614f34414650f2b2
Lonnon Foster <lonnon@lonnon.com>
2019-02-22 17:07:33 -0800

    Add some spaces. Let the code breathe, man.

--- src/jquery.responsiveGallery.js
+++ src/jquery.responsiveGallery.js
@@ -5,8 +5,8 @@
   http://www.CodingSerf.com
 */
 
-;(function($){
-  $.fn.responsiveGallery = function(option){
+;(function($) {
+  $.fn.responsiveGallery = function(option) {
     let opts = $.extend({}, $.fn.responsiveGallery.defaults, option);  // 配置选项
     let $rgWrapper = this;
     let $rgItems = $rgWrapper.find('li');  // .responsiveGallery-item
@@ -20,7 +20,7 @@
     let isAnimating = false;
     let touchX = 0;
 
-    function getTransform3dCSS(tx,ty,ry,zIndex,opacity,visibility){
+    function getTransform3dCSS(tx, ty, ry, zIndex, opacity, visibility) {
       return {
         '-webkit-transform' : 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
         '-moz-transform'    : 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
@@ -33,7 +33,7 @@
       };
     }
 
-    function getTransform2dCSS(t,s,originX,originY,opacity,visibility){
+    function getTransform2dCSS(t, s, originX, originY, opacity, visibility) {
       return {
         '-webkit-transform'        : 'translate('+t+') scale('+s+')',
         '-moz-transform'           : 'translate('+t+') scale('+s+')',
@@ -50,7 +50,7 @@
       };
     }
 
-    function getTransitionCSS(time,ease){
+    function getTransitionCSS(time, ease) {
       return {
         '-webkit-transition' : 'all '+time+'s '+ease,
         '-moz-transition'    : 'all '+time+'s '+ease,
@@ -60,10 +60,10 @@
       };
     }
 
-    function getTransform7CSS(){
+    function getTransform7CSS() {
       var cssArray;
 
-      if(support3d){
+      if (support3d) {
         cssArray = [
           getTransform3dCSS('-100%', -400,  40, -1, 0, 'hidden'),
           getTransform3dCSS(    '0', -350,  45, -1, 1, 'visible'),
@@ -75,7 +75,7 @@
           getTransform3dCSS( '600%', -350, -45, -1, 1, 'visible'),
           getTransform3dCSS( '700%', -400, -40, -1, 0, 'hidden')
         ];
-      } else if(support2d){
+      } else if (support2d) {
         cssArray = [
           getTransform2dCSS('-100%', 0.6, '100%', '50%', 0, 'hidden'),
           getTransform2dCSS(    '0', 0.6, '100%', '50%', 1, 'visible'),
@@ -92,10 +92,10 @@
       return cssArray;
     }
 
-    function getTransform5CSS(){
+    function getTransform5CSS() {
       var cssArray;
 
-      if(support3d){
+      if (support3d) {
         cssArray = [
           getTransform3dCSS('-100%', -400,  45, -1, 0, 'hidden'),
           getTransform3dCSS(    '0', -300,  45,  0, 1, 'visible'),
@@ -105,7 +105,7 @@
           getTransform3dCSS( '400%', -300, -45,  0, 1, 'visible'),
           getTransform3dCSS( '500%', -400, -45, -1, 0, 'hidden')
         ];
-      } else if(support2d){
+      } else if (support2d) {
         cssArray = [
           getTransform2dCSS('-100%', 0.6, '100%', '50%', 0, 'hidden'),
           getTransform2dCSS(    '0', 0.7, '100%', '50%', 1, 'visible'),
@@ -120,10 +120,10 @@
       return cssArray;
     }
 
-    function getTransform3CSS(){
+    function getTransform3CSS() {
       var cssArray;
 
-      if(support3d){
+      if (support3d) {
         cssArray = [
           getTransform3dCSS('-100%', -400,  45, 0, 0, 'hidden'),
           getTransform3dCSS(    '0', -300,  45, 1, 1, 'visible'),
@@ -131,7 +131,7 @@
           getTransform3dCSS( '200%', -300, -45, 1, 1, 'visible'),
           getTransform3dCSS( '300%', -400, -45, 0, 0, 'hidden')
         ];
-      } else if(support2d){
+      } else if (support2d) {
         cssArray = [
           getTransform2dCSS('-100%', 0.65, '100%', '50%', 0, 'hidden'),
           getTransform2dCSS(    '0',  0.8,  '50%', '50%', 1, 'visible'),
@@ -144,16 +144,16 @@
       return cssArray;
     }
 
-    function getTransform1CSS(){
+    function getTransform1CSS() {
       var cssArray;
 
-      if(support3d){
+      if (support3d) {
         cssArray = [
           getTransform3dCSS('-100%', -300,  45, 0, 0, 'hidden'),
           getTransform3dCSS(   '0%',    0,   0, 2, 1, 'visible'),
           getTransform3dCSS( '100%', -300, -45, 0, 0, 'hidden')
         ];
-      } else if(support2d){
+      } else if (support2d) {
         cssArray = [
           getTransform2dCSS('-100%', 0.65, '100%', '50%', 0, 'hidden'),
           getTransform2dCSS(    '0',    1,  '50%', '50%', 1, 'visible'),
@@ -163,79 +163,79 @@
 
       return cssArray;
     }
-    function setSectionItems(fun){
+    function setSectionItems(fun) {
       var $items = [];
 
       $items[0] = $rgItems.eq(rgCurrentIndex-1);
       fun(0,$items[0]);
-      for (var i=1;i<=rgShowCount+1;i++) {
-        var next = rgCurrentIndex+i-1;
-        if(next>=rgItemsLength){
+      for (var i=1; i<=rgShowCount+1; i++) {
+        var next = rgCurrentIndex + i - 1;
+        if (next >= rgItemsLength) {
           next = next - rgItemsLength;
         }
         $items[i] = $rgItems.eq(next);
-        fun(i,$items[i]);
+        fun(i, $items[i]);
       }
     }
 
-    function moveGallery(direction){
+    function moveGallery(direction) {
       isAnimating = true;
 
       rgCurrentIndex = direction + rgCurrentIndex;
-      if(rgCurrentIndex < 0){
+      if (rgCurrentIndex < 0) {
         rgCurrentIndex = rgItemsLength - 1;
       }
-      if(rgCurrentIndex >= rgItemsLength){
+      if (rgCurrentIndex >= rgItemsLength) {
         rgCurrentIndex = 0;
       }
-      setSectionItems(function(i,$item){
+      setSectionItems(function(i, $item) {
         $item.css(rgTansCSS[i]);
       });
 
-      setTimeout(function(){
+      setTimeout(function() {
         isAnimating = false;
       },animatDuration);  // 防止连击
     }
 
-    opts.$btn_next.on('click',function(e){
+    opts.$btn_next.on('click', function(e) {
       !isAnimating && moveGallery(+1);
 
     });
-    opts.$btn_prev.on('click',function(e){
+    opts.$btn_prev.on('click', function(e) {
       !isAnimating && moveGallery(-1);
 
     });
 
-    $rgWrapper.on('touchstart',function(e){
+    $rgWrapper.on('touchstart', function(e) {
       var touch = e.originalEvent.touches[0];
       touchX = touch.pageX;
-    }).on('touchend touchcancel',function(e){
+    }).on('touchend touchcancel', function(e) {
       var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0],
         touchGap = touch.pageX - touchX;
 
-      if(touchGap>5){  // swipe right
+      if (touchGap>5) {  // swipe right
         opts.$btn_prev.trigger('click');
       }
-      if(touchGap<-5){  // swipe left
+      if (touchGap<-5) {  // swipe left
         opts.$btn_next.trigger('click');
       }
 
     });
-    $(window).on('resize', function(e){
+    $(window).on('resize', function(e) {
       $rgItems.removeAttr('style');  // 清除样式，否则在resize的时候不能正常取到下列元素的width
 
       var wrapperWidth = $rgWrapper.width(),
         itemWidth = $rgItems.eq(0).width();
 
-      rgShowCount = Math.round(wrapperWidth/itemWidth);
+      rgShowCount = Math.round(wrapperWidth / itemWidth);
 
-      if(rgShowCount === 1){  // 显示1个
+      if (rgShowCount === 1) {  // 显示1个
         rgTansCSS = getTransform1CSS();
-      } else if(rgShowCount === 3){  // 显示3个
+      } else if (rgShowCount === 3) {  // 显示3个
         rgTansCSS = getTransform3CSS();
-      } else if(rgShowCount === 5){  // 显示5个
+      } else if (rgShowCount === 5) {  // 显示5个
         rgTansCSS = getTransform5CSS();
-      } else if(rgShowCount === 7){
+      } else if (rgShowCount === 7) {
         rgTansCSS = getTransform7CSS();
       } else {
         return;
@@ -243,9 +243,9 @@
 
       rgCurrentIndex = 0;
       moveGallery(0);
-      setTimeout(function(){
-        $rgItems.css(getTransitionCSS(animatDuration/1000, 'ease-in-out'));
-      },10);  // 等初始位置设置好后再添加动画
+      setTimeout(function() {
+        $rgItems.css(getTransitionCSS(animatDuration / 1000, 'ease-in-out'));
+      }, 10);  // 等初始位置设置好后再添加动画
 
     }).trigger('resize');
 

48b3a720d9f7ace4ea714c6cb1232fa6ae111e09
Lonnon Foster <lonnon@lonnon.com>
2019-02-26 10:30:34 -0800

    Add ability to go directly to a carousel item
    Animations are a bit odd when crossing the end/beginning of the items
    array, but everything is working acceptably. Except for the defaults,
    which never worked in the original code.
    
    Still needed:
    
    * Support for defining which event to handle on goto elements. Currently
      only binds to 'click', will need 'mouseover'.
    
    * Get vimeo embeds to work inside a carousel item. Might already be
      implemented in other code on the site, but not sure until I try the new
      carousel code in-place. Gotta love testing in production.

--- src/jquery.responsiveGallery.js
+++ src/jquery.responsiveGallery.js
@@ -15,11 +15,12 @@
     let support2d = Modernizr.csstransforms;
     let rgCurrentIndex = 0;
     let rgShowCount = 5;
-    let rgTansCSS = [];
+    let rgTransCSS = [];
     let animatDuration = opts.animatDuration;
     let isAnimating = false;
     let touchX = 0;
 
+    console.log(opts);
     function getTransform3dCSS(tx, ty, ry, zIndex, opacity, visibility) {
       return {
         '-webkit-transform' : 'translateX('+tx+') translateZ('+ty+'px) rotateY('+ry+'deg)',
@@ -131,7 +132,7 @@
           getTransform3dCSS( '199%', -300, -45, 1, 1, 'visible'),
           getTransform3dCSS( '300%', -400, -45, 0, 0, 'hidden')
         ];
-      } else if(support2d){
+      } else if (support2d){
         cssArray = [
           getTransform2dCSS('-100%', 0.65, '100%', '50%', 0, 'hidden'),
           getTransform2dCSS('-36px',  0.8,  '50%', '50%', 1, 'visible'),
@@ -163,12 +164,13 @@
 
       return cssArray;
     }
+
     function setSectionItems(fun) {
       var $items = [];
 
-      $items[0] = $rgItems.eq(rgCurrentIndex-1);
+      $items[0] = $rgItems.eq(rgCurrentIndex - 1);
       fun(0,$items[0]);
-      for (var i=1; i<=rgShowCount+1; i++) {
+      for (var i = 1; i <= rgShowCount + 1; i++) {
         var next = rgCurrentIndex + i - 1;
         if (next >= rgItemsLength) {
           next = next - rgItemsLength;
@@ -178,32 +180,71 @@
       }
     }
 
-    function moveGallery(direction) {
-      isAnimating = true;
-
-      rgCurrentIndex = direction + rgCurrentIndex;
-      if (rgCurrentIndex < 0) {
-        rgCurrentIndex = rgItemsLength - 1;
+    function moveGallery(direction, distance) {
+      let moveDistance = 1;
+      if (distance !== undefined) {
+        moveDistance = distance;
       }
-      if (rgCurrentIndex >= rgItemsLength) {
-        rgCurrentIndex = 0;
-      }
-      setSectionItems(function(i, $item) {
-        $item.css(rgTansCSS[i]);
-      });
 
-      setTimeout(function() {
-        isAnimating = false;
-      },animatDuration);  // 防止连击
+      if (moveDistance === 0) {
+        setSectionItems(function(i, $item) {
+          $item.css(rgTransCSS[i]);
+        });
+      }
+
+      for (let i = 0; i < moveDistance; i++) {
+        isAnimating = true;
+
+        rgCurrentIndex += direction;
+        if (rgCurrentIndex < 0) {
+          rgCurrentIndex = (rgCurrentIndex % rgItemsLength) + rgItemsLength;
+        }
+        if (rgCurrentIndex >= rgItemsLength) {
+          rgCurrentIndex = rgCurrentIndex % rgItemsLength;
+        }
+        setSectionItems(function(i, $item) {
+          $item.css(rgTransCSS[i]);
+        });
+
+        setTimeout(function() {
+          isAnimating = false;
+        }, animatDuration);  // 防止连击
+      }
+    }
+
+    function getShortestMovement(from, to, len) {
+      if (from === to) {
+        return [0, 0];
+      }
+      const innerDistance = Math.max(from, to) - Math.min(from, to);
+      const outerDistance = len- innerDistance;
+      const shortestDistance = Math.min(innerDistance, outerDistance);
+      const shortestDistanceIsInternal = innerDistance < len/ 2;
+      if ((shortestDistanceIsInternal && from < to) ||
+          (!shortestDistanceIsInternal && from > to)) {
+        return [+1, shortestDistance];
+      } else {
+        return [-1, shortestDistance];
+      }
+    }
+
+    function gotoGallery(index) {
+      const [direction, distance] = getShortestMovement(rgCurrentIndex, index, rgItemsLength);
+      moveGallery(direction, distance);
     }
 
     opts.$btn_next.on('click', function(e) {
       !isAnimating && moveGallery(+1);
-
     });
+
     opts.$btn_prev.on('click', function(e) {
       !isAnimating && moveGallery(-1);
+    });
 
+    opts.$btn_goto.on('click', function(e) {
+      console.log(e);
+      console.log(e.target.value);
+      gotoGallery(e.target.value);
     });
 
     $rgWrapper.on('touchstart', function(e) {
@@ -219,30 +260,30 @@
       if (touchGap<-5) {  // swipe left
         opts.$btn_next.trigger('click');
       }
-
     });
+
     $(window).on('resize', function(e) {
       $rgItems.removeAttr('style');  // 清除样式，否则在resize的时候不能正常取到下列元素的width
 
-      var wrapperWidth = $rgWrapper.width(),
-        itemWidth = $rgItems.eq(0).width();
+      let wrapperWidth = $rgWrapper.width();
+      let itemWidth = $rgItems.eq(0).width();
 
       rgShowCount = Math.round(wrapperWidth / itemWidth);
 
       if (rgShowCount === 1) {  // 显示1个
-        rgTansCSS = getTransform1CSS();
+        rgTransCSS = getTransform1CSS();
       } else if (rgShowCount === 3) {  // 显示3个
-        rgTansCSS = getTransform3CSS();
+        rgTransCSS = getTransform3CSS();
       } else if (rgShowCount === 5) {  // 显示5个
-        rgTansCSS = getTransform5CSS();
+        rgTransCSS = getTransform5CSS();
       } else if (rgShowCount === 7) {
-        rgTansCSS = getTransform7CSS();
+        rgTransCSS = getTransform7CSS();
       } else {
         return;
       }
 
       rgCurrentIndex = 0;
-      moveGallery(0);
+      moveGallery(0, 0);
       setTimeout(function() {
         $rgItems.css(getTransitionCSS(animatDuration / 1000, 'ease-in-out'));
       }, 10);  // 等初始位置设置好后再添加动画
@@ -252,9 +293,12 @@
     // 链式返回
     return this;
   };
+
+  // FIXME: defaults don't work!
   $.fn.responsiveGallery.defaults = {
     animatDuration: 400,  // 动画时长 单位 ms
     $btn_prev: $('.responsiveGallery-btn_prev'),
-    $btn_next: $('.responsiveGallery-btn_next')
+    $btn_next: $('.responsiveGallery-btn_next'),
+    $btn_goto: $('.responsiveGallery-btn_goto')
   };
 })(jQuery);
